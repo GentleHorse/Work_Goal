2939c22b332e70ce4f38da751f06449ce7308832
Toshi E <anch.abc.t@gmail.com>
2023-07-25 21:22:58 +0900

    Finished base structure

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React from "react";
+import styles from "./Button.module.css";
 
-import './Button.css';
+// import styled from 'styled-components';
+
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 0.5px solid  rgba(0, 0, 0, 0.05);
+//   color: white;
+//   background: rgba(0, 0, 0, 0.26);
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
+
+//   &:focus {
+//   outline: none;
+//   }
+
+// &:hover,
+// &:active {
+//   background: #f98bd5;
+//   border-color:  #f98bd5;
+//   box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
